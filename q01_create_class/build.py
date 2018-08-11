c7539d96956be4b7bcebf918c594b99f564f0da1
raahuul <er.rahul.v27@gmail.com>
2018-08-11 12:59:45 +0000

    Done

--- q01_create_class/build.py
+++ q01_create_class/build.py
@@ -2,14 +2,41 @@ import pandas as pd
 import numpy as np
 import math
 
-"write your solution here"
 
 
 class complex_number:
-    """The complex number class.
+    def __init__(self,real,imag):
+        self.real=real
+        self.imag=imag
+    
+    def __str__(self):
+        return '{:d}{:+d}j'.format(self.real,self.imag)
+    
+    def __add__(self,other):
+        return complex_number((self.real+other.real),(self.imag+other.imag))
+    
+    def __sub__(self,other):
+        return complex_number((self.real-other.real),(self.imag-other.imag))
+    
+    def __mul__(self,other):
+        real=((self.real*other.real)-(self.imag*other.imag))
+        imag=((self.real*other.imag)+(self.imag*other.real))
+        return complex_number(real,imag)
+    
+    def __truediv__(self,other):
+        
+        real=((self.real*other.real)+(self.imag*other.imag))/(other.real**2+other.imag**2)         
+        imag=((self.imag*other.real)-(self.real*other.imag))/(other.real**2+other.imag**2)
+        return (real,imag)
+    
+    def abs(self):
+        return math.sqrt(self.real**2+self.imag**2)
+    
+    def conjugate(self):
+        return complex_number(self.real,-(self.imag))
+    
+    def argument(self):
+        return math.degrees(math.atan(self.imag/self.real))
+        
 
-    Attributes:
-    attr1 (x): Real part of complex number.
-    attr2 (y): Imaginary part of complex number.
 
-    """
